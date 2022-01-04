d267738175bfa46b688f5d81dfe02bd77cf0a73c
Harel Shetu <harelshetu@gmail.com>
2022-01-04 21:10:02 +0200

    add

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import react, { useState } from "react";
+
+const Weather = (WeatherDetails)=>{
+
+
+    return (
+        <div>
+          <h2> city:</h2>
+          <h4>Date:</h4>
+          <h4>Hour:</h4>
+          <h2> image:</h2>
+          <h2> temp:</h2>
+          <h2> humidity:</h2>
+        </div>
+      );
+}
+
+export default Weather;
